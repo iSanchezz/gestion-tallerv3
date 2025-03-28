bdd39e337887912dc52571e51afc9b927fd69991
iSanchezz <j.sangon16@gmail.com>
2025-03-28 11:51:25 +0100

    update

--- Taller/src/dao/InsertarDB.java
+++ Taller/src/dao/InsertarDB.java
@@ -3,22 +3,6 @@ package dao;
 import java.sql.*;
 
 public class InsertarDB {
-    public static void agregarCliente(String nombre, String telefono, String direccion) {
-        Connection conexion = ConexionDB.conectar();
-        if (conexion != null) {
-        String query = "INSERT INTO clientes (nombre, telefono, direccion) VALUES (?, ?, ?)";
 
-            try (PreparedStatement stmt = conexion.prepareStatement(query)) {
-            stmt.setString(1, nombre); // Asigna el valor del nombre
-            stmt.setString(2, telefono); // Asigna el valor del teléfono
-            stmt.setString(3, direccion); // Asigna la dirección
-            stmt.executeUpdate(); // Ejecuta la consulta de inserción
-            System.out.println("Cliente agregado exitosamente.");
-            
-            } catch (SQLException e) {
-            System.out.println("Error al agregar cliente: " + e.getMessage());
-            }
-        }
-    }
 
 }

18285885608d420c4a677e6a4f5b022dda9855a9
iSanchezz <j.sangon16@gmail.com>
2025-03-28 11:50:13 +0100

    update

--- Taller/src/dao/ConexionDB.java
+++ Taller/src/dao/ConexionDB.java
@@ -1,5 +1,27 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+     // URL de conexión a la base de datos MySQL
+ private static final String URL = "jdbc:mysql://127.0.0.1:3306/taller";
+ 
+ private static final String USUARIO = "Jorge"; // Nombre de usuario de 
+//MySQL
+ private static final String CONTRASENA = "debian"; // 
+//Contraseña del usuario de MySQL
+ // Método para establecer la conexión con la base de datos
+ public static Connection conectar() {
+    try {
+        // Establecer la conexión con la base de datos
+        return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+    } catch (SQLException e) {
+        System.out.println("Error al conectar a la base de datos: " + 
+        e.getMessage());
+        return null;
+    }
+ }
 
 }

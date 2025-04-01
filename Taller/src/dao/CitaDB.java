d3b97f684393e2b24180e6bcdd04bb91dd0bbbaa
JoelDiazGH <jxeldiazihs@gmail.com>
2025-04-01 13:49:36 +0200

    generarnumcita

--- Taller/src/dao/CitaDB.java
+++ Taller/src/dao/CitaDB.java
@@ -1,5 +1,29 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CitaDB {
     
+
+
+
+public int generarNumCita(){
+     Connection conexion = dao.ConexionDB.conectar();
+
+    String query="SELECT MAX(numerocita) FROM citas";
+
+     try(Statement stmt = conexion.createStatement();
+            ResultSet rs=stmt.executeQuery(query)){
+            
+                   return rs.getInt(query);
+
+            }catch(SQLException e){
+                System.out.println("error al realizar la consulta"+ e.getMessage());
+                
+            }
+            return 0;
+    }
 }

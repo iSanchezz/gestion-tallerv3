3fdc7c243b46665481b766b1dcdf225794390972
iSanchezz <j.sangon16@gmail.com>
2025-04-02 09:24:19 +0200

    Clase Cita

--- Taller/src/model/Cita.java
+++ Taller/src/model/Cita.java
@@ -1,15 +1,35 @@
 package model;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Cita {
     int numeroCita;
     LocalDate fecha;
-    LocalDateTime hora;
+    LocalTime hora;
+    String idVehiculo;
 
-    public Cita(int numeroCita, LocalDate fecha, LocalDateTime hora) {
+    public Cita(int numeroCita, LocalDate fecha, LocalTime hora, String idVehiculo) {
         this.numeroCita = numeroCita;
         this.fecha = fecha;
         this.hora = hora;
+        this.idVehiculo = idVehiculo;
     }
+
+    public int getNumeroCita(){
+        return this.numeroCita;
+    }
+
+    public LocalDate getFecha(){
+        return this.fecha;
+    }
+
+    public LocalTime getHora(){
+        return this.hora;
+    }
+
+    public String getIdVehiculo(){
+        return this.idVehiculo;
+    }
+    
+    
 }

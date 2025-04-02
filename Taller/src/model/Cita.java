11b24c43006d9e863795c0c0e2af10914f0d984a
iSanchezz <j.sangon16@gmail.com>
2025-04-02 08:38:54 +0200

    update

--- Taller/src/model/Cita.java
+++ Taller/src/model/Cita.java
@@ -3,10 +3,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Cita {
+    int numeroCita;
     LocalDate fecha;
     LocalDateTime hora;
 
-    public Cita(LocalDate fecha, LocalDateTime hora) {
+    public Cita(int numeroCita, LocalDate fecha, LocalDateTime hora) {
+        this.numeroCita = numeroCita;
         this.fecha = fecha;
         this.hora = hora;
     }
